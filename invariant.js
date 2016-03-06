3e98f132e744a8880c4705817fd1bcd8254d12bc
Mihail Bodrov <connormiha1@gmail.com>
2016-03-06 15:50:30 +0300

    removed nesting conditions

--- invariant.js
+++ invariant.js
@@ -29,25 +29,27 @@ var invariant = function(condition, format, a, b, c, d, e, f) {
     }
   }
 
-  if (!condition) {
-    var error;
-    if (format === undefined) {
-      error = new Error(
-        'Minified exception occurred; use the non-minified dev environment ' +
-        'for the full error message and additional helpful warnings.'
-      );
-    } else {
-      var args = [a, b, c, d, e, f];
-      var argIndex = 0;
-      error = new Error(
-        format.replace(/%s/g, function() { return args[argIndex++]; })
-      );
-      error.name = 'Invariant Violation';
-    }
-
-    error.framesToPop = 1; // we don't care about invariant's own frame
-    throw error;
+  if (condition) {
+    return;
   }
+
+  var error;
+  if (format === undefined) {
+    error = new Error(
+      'Minified exception occurred; use the non-minified dev environment ' +
+      'for the full error message and additional helpful warnings.'
+    );
+  } else {
+    var args = [a, b, c, d, e, f];
+    var argIndex = 0;
+    error = new Error(
+      format.replace(/%s/g, function() { return args[argIndex++]; })
+    );
+    error.name = 'Invariant Violation';
+  }
+
+  error.framesToPop = 1; // we don't care about invariant's own frame
+  throw error;
 };
 
 module.exports = invariant;
